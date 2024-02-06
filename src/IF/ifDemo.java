464eedf8a73dfcbd82ea4c05832a3f5e20551ae8
Malvika <malvikarasotra@gmail.com>
2024-02-06 15:24:35 +0530

    first commit

---
+++ src/IF/ifDemo.java
@@ -0,0 +1,22 @@
+package IF;
+
+public class ifDemo {
+	public static void main(String[] args) {  
+		//defining a variable  
+	    int number=12;  
+	    //Check if the number is divisible by 2 or not  
+	    if(number%2==0){  
+	        System.out.println("even number");  
+	    }else{  
+	        System.out.println("odd number");  
+	        System.out.println("hey this is demo"); 
+	    }  
+	}  
+	  
+}
+	
+	
+
+
+	
+
---
+++ src/Variable/VarDemo.java
@@ -0,0 +1,28 @@
+package Variable;
+
+public class VarDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int a = 26;
+		String name = "Malvika";
+		float myFloatNum = 5.22f;
+		char myLetter = 'D';
+		boolean myBool = true;
+		
+		
+		System.out.println(a);
+		System.out.println(a - 2);
+		System.out.println(a + 5);
+		System.out.println(a /2);
+		System.out.println(a *3); 
+		System.out.println(name);
+		System.out.println(myFloatNum);
+		System.out.println(myLetter);
+		System.out.println(myBool);
+		
+		
+	}
+
+}
